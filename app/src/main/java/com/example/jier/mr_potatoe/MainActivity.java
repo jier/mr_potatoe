7b89b500396124d3479f7eb21aa20e93a2d90617
jier <jiernzuanzu@gmail.com>
2018-02-09 17:52:00 +0100

    Update

--- app/src/main/java/com/example/jier/mr_potatoe/MainActivity.java
+++ app/src/main/java/com/example/jier/mr_potatoe/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.jier.mr_potatoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void checkClicked(View view) {
+        CheckBox checkbox = (CheckBox) view;
+        String checkTxt  = "";
+
+//        ImageView img = (ImageView) findViewById(R.id.arms);
+        if (checkbox.isChecked()) {
+            checkTxt = checkbox.getText().toString();
+            System.out.println(checkTxt);
+            Log.d("potato", "checkTxt" );
+        }else {
+            System.out.println(checkTxt);
+        }
+
+        Log.d("potato", "checkClicked:");
+
+    }
+
 }
